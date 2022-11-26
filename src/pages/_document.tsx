22df95e767c46562b5ea38f5e22b3d1483d8a0e3
gfrnca <gabrielbfranca27@gmail.com>
2022-11-26 04:05:52 -0300

    Change page title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ export default class CustomDocument extends Document {
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet" />
 
-                    <title>Interbank</title>
+                    <title>Intrabank</title>
 
                     <style id="stitches" dangerouslySetInnerHTML={{__html: getCssText()}} />
                 </Head>
