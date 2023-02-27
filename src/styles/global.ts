c1bc3966f680343d8de37006b854bece6d57801f
gfrnca <gabrielbfranca27@gmail.com>
2023-02-27 16:43:15 -0300

    Created theme file

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,11 @@
 import { globalCss } from '@stitches/react'
+import { styled, css } from './theme'
+
 
 export const globalStyles = globalCss({
-  '*': { margin: 0, padding: 0, boxSizing: 'border-box', fontFamily: 'Poppins' }
+  '*': { margin: 0, padding: 0, boxSizing: 'border-box', fontFamily: 'Poppins' },
+
+  'body': {
+    backgroundColor: '$gray200',
+  }
 })
