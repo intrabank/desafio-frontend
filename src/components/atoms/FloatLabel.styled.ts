e89d3bc30095434dbe302ee209b9a564836c36b8
gfrnca <gabrielbfranca27@gmail.com>
2023-02-27 19:16:03 -0300

    Improved float label effect

--- src/components/atoms/FloatLabel.styled.ts
+++ src/components/atoms/FloatLabel.styled.ts
@@ -7,7 +7,7 @@ export const FloatLabelContainer = styled("div", {
     background: "$dark950",
     border: "none",
     borderRadius: "8px",
-    padding: "19px 16px",
+    padding: "24px 16px 10px 16px",
     width: "100%",
     outline: "1px solid $dark800",
     color: "#FFF",
@@ -20,7 +20,7 @@ export const FloatLabelContainer = styled("div", {
 
   ".input-label": {
     position: "absolute",
-    top: "17px",
+    top: "14px",
     left: "16px",
     transition: "all 0.2s ease-out",
     pointerEvents: "none",
@@ -28,12 +28,12 @@ export const FloatLabelContainer = styled("div", {
   },
 
   ".input-control:focus + .input-label": {
-    top: "0px",
+    top: "4px",
     fontSize: "12px",
   },
 
   ".input-label.float": {
-    top: "0px",
+    top: "4px",
     fontSize: "0.75rem",
   },
 });
